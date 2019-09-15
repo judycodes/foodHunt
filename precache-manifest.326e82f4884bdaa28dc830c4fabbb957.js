365d2590c5a7a5f20f8519c5603aa62cd05e89dc
judycodes <43575215+judycodes@users.noreply.github.com>
2019-09-15 17:11:27 -0400

    Updates

---
+++ precache-manifest.326e82f4884bdaa28dc830c4fabbb957.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7a30990a82e7a933fe47ce2db2eabf",
+    "url": "/foodHunt/index.html"
+  },
+  {
+    "revision": "fb89450a6e7948bf296a",
+    "url": "/foodHunt/static/css/main.0771f0fb.chunk.css"
+  },
+  {
+    "revision": "3165215b60029a4891fb",
+    "url": "/foodHunt/static/js/2.837ff56e.chunk.js"
+  },
+  {
+    "revision": "fb89450a6e7948bf296a",
+    "url": "/foodHunt/static/js/main.db549406.chunk.js"
+  },
+  {
+    "revision": "b5f88fc7b415aca7645c",
+    "url": "/foodHunt/static/js/runtime~main.bdd31fb4.js"
+  },
+  {
+    "revision": "9ea57640dddc27ac8e1925e9f9ae27a7",
+    "url": "/foodHunt/static/media/Yelp_trademark_RGB.9ea57640.png"
+  },
+  {
+    "revision": "8fce54eb22ff093b9a2b835c6eea4683",
+    "url": "/foodHunt/static/media/aboutbg.8fce54eb.jpg"
+  },
+  {
+    "revision": "ceefdc7b57fc616ef277f986f4f5c665",
+    "url": "/foodHunt/static/media/cooking.ceefdc7b.jpg"
+  },
+  {
+    "revision": "35665d8e8f070c15efd4da8f9f809518",
+    "url": "/foodHunt/static/media/fire.35665d8e.jpg"
+  },
+  {
+    "revision": "7b23134d357bcf803f519c49d0ea1dff",
+    "url": "/foodHunt/static/media/hangry.7b23134d.png"
+  },
+  {
+    "revision": "25c951af86eadbead5aae7e06965d895",
+    "url": "/foodHunt/static/media/hangryplaceholder.25c951af.jpg"
+  },
+  {
+    "revision": "d0ddb0379332e63d198f8b2b317e6488",
+    "url": "/foodHunt/static/media/homeplaceholder.d0ddb037.jpg"
+  },
+  {
+    "revision": "66828086098a230c23d34a96cc0153d2",
+    "url": "/foodHunt/static/media/kitchenplaceholder.66828086.jpg"
+  },
+  {
+    "revision": "fdd2e1408ac36659d30b1263a64cd0ab",
+    "url": "/foodHunt/static/media/landing-bg.fdd2e140.png"
+  },
+  {
+    "revision": "88d6aeb5f0804a9a59981389b0f6d099",
+    "url": "/foodHunt/static/media/landingplaceholder.88d6aeb5.jpg"
+  },
+  {
+    "revision": "95c09b2d58dc6830edcf57e1aeeb3347",
+    "url": "/foodHunt/static/media/restaurantbg.95c09b2d.jpg"
+  },
+  {
+    "revision": "890a88dfe46340ba36ec2dd1dbabfec2",
+    "url": "/foodHunt/static/media/yelpimgplaceholder.890a88df.jpg"
+  },
+  {
+    "revision": "506d0cef263fd3ca4cd5e9b23a4f6ab5",
+    "url": "/foodHunt/static/media/yumplaceholder.506d0cef.jpg"
+  }
+]);
